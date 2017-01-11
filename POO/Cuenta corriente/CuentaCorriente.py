6c521a50c91693f8841ab31d3a21636d789f6388
Guillermo <guille.mca@hotmail.com>
2017-01-11 19:43:51 +0100

    Corregido ejercicio Cuenta Corriente -eliminada Interfaz

--- POO/Cuenta corriente/CuentaCorriente.py
+++ POO/Cuenta corriente/CuentaCorriente.py
@@ -19,61 +19,47 @@
 ####ingresarDinero(): añade al saldo una cantidad de dinero.
 ####consultarCuenta(): visualizará los datos de la cuenta.
 ####saldoNegativo(): devolverá un valor lógico indicando si la cuenta está o no en números rojos.
-from creaMatrizCasosTest import *
 
-class DatosCuentaCorriente:
+class CuentaCorriente:
 	def __init__(self,nombre,apellidos,direccion,telefono,nif,saldo):
-		self.nombre=nombre
-		self.apellidos=apellidos
-		self.direccion=direccion
-		self.telefono=telefono
-		self.nif=nif
-		self.saldo=saldo
+		self.__nombre=nombre
+		self.__apellidos=apellidos
+		self.__direccion=direccion
+		self.__telefono=telefono
+		self.__nif=nif
+		self.__saldo=saldo
+	#SETTERS
+	def setNombre(self, nombre):
+		self.__nombre=nombre
+	def setApellidos(self, apellidos):
+		self.__apellidos=apellidos
+	def setDireccion(self, direccion):
+		self.__direccion=direccion
+	def setTelefono(self, telefono):
+		self.__telefono=telefono
+	def setNif(self, nif):
+		self.__nif=nif
+	def setSaldo(self, saldo):
+		self.__saldo=saldo
+	#GETTERS
+	def getNombre(self):
+		return self.__nombre
+	def getApellidos(self):
+		return self.__apellidos
+	def getDireccion(self):
+		return self.__direccion
+	def getTelefono(self):
+		return self.__telefono
+	def getNif(self):
+		return self.__nif
+	def getSaldo(self):
+		return self.__saldo
 
 	def __str__(self):
-		return '[Propietario: %s %s, Direccion: %s, Telefono: %s, NIF: %s, Saldo Disponible: %s]' %(self.nombre, self.apellidos, self.direccion, self.telefono, self.nif, self.saldo)
-
-class InterfazCuentaCorriente():
-	def setNombre(self, nombre):
-		self.nombre=nombre
-	def setApellidos(self, apellidos):
-		self.apellidos=apellidos
-	def setDireccion(self, direccion):
-		self.direccion=direccion
-	def setTelefono(self, telefono):
-		self.telefono=telefono
-	def setNif(self, nif):
-		self.nif=nif
-	def setSaldo(self, saldo):
-		self.saldo=saldo
-
-	def getNombre(self):
-		return self.nombre
-	def getApellidos(self):
-		return self.apellidos
-	def getDireccion(self):
-		return self.direccion
-	def getTelefono(self):
-		return self.telefono
-	def getNif(self):
-		return self.nif
-	def getSaldo(self):
-		return self.saldo
-
-
-	def retirarDinero(self, cantidad):
-		if cantidad>self.saldo:
-			print ("No dispone de tal cantidad para retirar")
-			print ("Su saldo disponible es de", self.saldo)
-		else:
-			self.saldo=self.saldo-cantidad
-			print ("Ha retirado %s€, su saldo disponible es de %s€" %(cantidad, self.saldo))
-
-	def ingresarDinero(self, cantidad):
-		self.saldo+=cantidad
+		return '[Propietario: %s %s, Direccion: %s, Telefono: %s, NIF: %s, Saldo Disponible: %s]' %(self.__nombre, self.__apellidos, self.__direccion, self.__telefono, self.__nif, self.__saldo)
 
 	def saldoNegativo(self):
-		if self.saldo<0:
+		if self.__saldo<0:
 			return True
 		else:
 			return False
@@ -81,18 +67,28 @@ class InterfazCuentaCorriente():
 	def consultarCuenta(self):
 		print(self)
 
-class CuentaCorriente(InterfazCuentaCorriente, DatosCuentaCorriente):
-	def __init__(self, nombre, apellidos, direccion, telefono, nif, saldo):
-		DatosCuentaCorriente.__init__(self, nombre, apellidos, direccion, telefono, nif, saldo)
+	def ingresarDinero(self, cantidad):
+		self.__saldo+=cantidad
 
-class Cliente(CuentaCorriente):
-	def __init__(self, nombre, apellidos, direccion, telefono, nif, saldo):
-		DatosCuentaCorriente.__init__(self, nombre, apellidos, direccion, telefono, nif, saldo)
+	def retirarDinero(self, cantidad):
+		if cantidad>self.__saldo:
+			print ("No dispone de tal cantidad para retirar")
+			print ("Su saldo disponible es de", self.saldo)
+		else:
+			self.__saldo=self.__saldo-cantidad
+			print ("Ha retirado %s€, su saldo disponible es de %s€" %(cantidad, self.__saldo))
+
+
 
 
 
 if __name__=='__main__':
-	guilleCM = Cliente("Guillermo", "Cirer Martorell", "C/ Tenor Bou Roig", 717114964, "43223381X", 60)
+	guilleCM = CuentaCorriente("Guillermo", "Cirer Martorell", "C/ Tenor Bou Roig", 717114964, "43223381X", 60)
 	guilleCM.ingresarDinero(40) #saldo pasa a ser 100
+	guilleCM.retirarDinero(100)
 	guilleCM.setNombre("Pedro") #nombre pasa a ser Pedro
 	guilleCM.consultarCuenta() #comprueba que lo anterior se ha cumplido
+	cuentaGuille = CuentaCorriente("Guillermo", "Cirer Martorell", "C/ Tenor Bou Roig", 717114964, "43223381X", 60)
+	print(cuentaGuille.getNombre())
+	print(cuentaGuille.setNif("re"))
+	print(cuentaGuille.getNif())
